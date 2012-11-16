3dcec243ee209ff90220057f6a6e402a5383ae32
dragon <rongjihuang@gmail.com>
2012-11-16 19:34:58 +0800

    树

--- uidesign/tree/tree-table.js
+++ uidesign/tree/tree-table.js
@@ -33,25 +33,87 @@ bc.tree = {
 	/** 展开折叠节点 */
 	toggleNode: function($node){
 		$node.toggleClass("open collapsed");
-		$node.find(">.item>.nav").toggleClass("ui-icon ui-icon-triangle-1-se ui-icon ui-icon-triangle-1-e");
-		$node.find(">.item>.type").toggleClass("ui-icon-folder-open ui-icon-folder-collapsed");
+		$node.find(">.item>.nav-icon").toggleClass("ui-icon ui-icon-triangle-1-se ui-icon ui-icon-triangle-1-e");
+		$node.find(">.item>.type-icon").toggleClass("ui-icon-folder-open ui-icon-folder-collapsed");
+	},
+	/** 获取选中的节点信息
+	 * @param $tree tree的jquery对象
+	 * @param returnJson true|false {id:..., name:...,el:...}
+	 */
+	getSelected: function($tree,returnJson){
+		var $items = $tree.find("div.item." + bc.tree.option.class_node_selected);
+		if($items.length == 1){
+			if(returnJson){
+				return {
+					id: $items.attr("data-id"),
+					name: $items.children(".text").text(),
+					el: $items.get(0)
+				};
+			}else{
+				return $items.attr("data-id");
+			}
+		}else if($items.length > 1){
+			var r = [];
+			$items.each(function(i){
+				var $t = $(this);
+				if(returnJson){
+					r.push({
+						id: $t.attr("data-id"),
+						name: $t.children(".text").text(),
+						el: $t.get(0)
+					});
+				}else{
+					r.push($t.attr("data-id"));
+				}
+			});
+			return r;
+		}else{
+			return null;
+		}
 	}
 };
 
-// 树：鼠标悬停样式
-$(".bc-tree .node>.item").live("mouseover mouseout click",function(e){
+// 节点的事件监听
+$(".treeNode>.item").live("mouseover mouseout click dblclick",function(e){
 	e.stopPropagation();
 	var $nodeItem = $(this);
 	var $node = $nodeItem.parent();
-	if (e.type == 'click') {										// 单击行
+	if (e.type == 'click') {										// 单击节点
+		console.log("click");
 		bc.tree.selectNode($node);
-		if ($(e.target).is(".nav:visible")){
+		if ($(e.target).is(".nav-icon:visible")){
 			bc.tree.toggleNode($node);
 		}
+		//console.log(bc.tree.getSelected($node.closest("." + bc.tree.option.class_container),true));
+		
+		// 调用回调函数
+		var $tree = $node.closest("." + bc.tree.option.class_container);
+		var cfg = $tree.data("cfg");
+		if(cfg && cfg.callback){
+			var cbFn = cfg.callback;
+			if(typeof(cfg.callback) == "string"){
+				cfg.callback = bc.getNested(cfg.callback);
+			}
+		}
+	}else if (e.type == 'dblclick') {								// 双击节点
+		console.log("dblclick:todo");
 	}else if (e.type == 'mouseover') {	// 鼠标悬停及离开行
 		$nodeItem.toggleClass(bc.tree.option.class_node_hover,true).toggleClass("ui-state-normal",false);
 	}else if (e.type == 'mouseout') {	// 鼠标悬停及离开行
 		$nodeItem.toggleClass(bc.tree.option.class_node_hover,false).filter(":not(." + bc.tree.option.class_node_selected + ")").toggleClass("ui-state-normal",true);
+	}
+});
+
+// 节点右侧操作按钮的时间监听
+$(".treeNode>.item>ul.buttons>li").live("mouseover mouseout click",function(e){
+	e.stopPropagation();
+	var $button = $(this);
+	var $node = $button.closest(".treeNode");
+	if (e.type == 'click') {										// 单击
+		console.log("click nodeButton");
+		// TODO
+	}else if (e.type == 'mouseover' || e.type == 'mouseout') {		// 鼠标悬停及离开行
+		$button.toggleClass("ui-state-hover ui-corner-all");
 	}
 });
 
