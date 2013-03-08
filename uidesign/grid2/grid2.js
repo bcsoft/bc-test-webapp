d6abd6febfedb456105b8bdb4c5190dde73059d2
dragon <rongjihuang@gmail.com>
2013-03-08 18:09:56 +0800

    grid2设计

---
+++ uidesign/grid2/grid2.js
@@ -0,0 +1,21 @@
+var bc={};
+bc.grid2 = {
+	/**
+	 * 默认配置
+	 */
+	option: {
+		
+	},
+	
+	/**
+	 * 初始化
+	 * @param container jquery对象
+	 */
+	init: function(container) {
+		
+	}
+};
+
+(function($) {
+	//bc.tree.init(".tree");
+})(jQuery);
