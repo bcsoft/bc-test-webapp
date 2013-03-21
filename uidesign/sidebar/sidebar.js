3e8e289b6bd24e3d0eeb5b72dc63ef4b74ed6f6b
dragon <rongjihuang@gmail.com>
2013-03-21 14:54:32 +0800

    sidebar设计1（table）

---
+++ uidesign/sidebar/sidebar.js
@@ -0,0 +1,12 @@
+var bc={};
+bc.sidebar = {
+	init: function() {
+		//alert("sidebar");
+	}
+};
+
+(function($) {
+	bc.sidebar.init(".sidebar");
+	//$g = $(".group");
+	//$g.parent().append($g.clone());
+})(jQuery);
